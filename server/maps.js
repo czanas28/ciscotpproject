aeeb056cc9ef42f218a5665e7a7c32652d36b254
czanas28 <60748022+czanas28@users.noreply.github.com>
2021-08-18 19:51:12 -0400

    Updated the maps for touch panel gui commands

--- server/maps.js
+++ server/maps.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const events = new Map;
+exports.panels = new Map();
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const panelFiles = fs
+  .readdirSync("./xapigui/panels")
+  .filter((file) => file.endsWith(".js"));
 
-for (const file of eventFiles) {
-    const evt = require(`./events/${file}`)
-    events.set(evt.name, evt)
+for (const file of panelFiles) {
+  const panel = require(`./xapigui/panels/${file}`);
+  panels.set(panel.name, panel);
 }
 
-exports.events = events;
+exports.widgets = new Map();
+const widgetFiles = fs
+  .readdirSync("./xapigui/widgets")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of widgetFiles) {
+  const widget = require(`./xapigui/widgets/${file}`);
+  widgets.set(widget.name, widget);
+}
