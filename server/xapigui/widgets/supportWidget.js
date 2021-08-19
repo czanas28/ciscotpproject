fb7e1f3fee92824de6e7e0e73d14b5e5745cb591
czanas28 <60748022+czanas28@users.noreply.github.com>
2021-08-18 22:10:30 -0400

    updated 'Text' on function to be clearer

--- server/xapigui/widgets/supportWidget.js
+++ server/xapigui/widgets/supportWidget.js
@@ -4,11 +4,10 @@ module.exports = {
   execute(xapi) {
     xapi.Command.UserInterface.Message.TextInput.Display({
       Duration: 0,
-      FeedbackId: 100,
-      Placeholder: "Enter info here",
+    //   FeedbackId: 100,
       SubmitText: "Submit",
-      Text: "<p>Enter your phone number or email address, so IT Support can contact you, then press Submit</p>",
-      Title: "Support Form",
+      Text: "<p>Enter your phone number or email address and IT Support will contact you shortly.</p>",
+      Title: "Support",
     });
     xapi.Event.UserInterface.Message.TextInput.Response.on((event) => {
       console.log(event);
